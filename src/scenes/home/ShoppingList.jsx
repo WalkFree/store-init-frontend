889d3726240b70abb3929d900fcc341f37cad365
Mohan <mohan.m074@gmail.com>
2023-09-01 16:51:55 -0400

    chaining

--- src/scenes/home/ShoppingList.jsx
+++ src/scenes/home/ShoppingList.jsx
@@ -31,13 +31,13 @@ const ShoppingList = () => {
     getItems();
   }, []);
 
-  const topRatedItems = items.filter(
+  const topRatedItems = items?.filter(
     (item) => item?.attributes?.category === "topRated"
   );
-  const newArrivalsItems = items.filter(
+  const newArrivalsItems = items?.filter(
     (item) => item?.attributes?.category === "newArrivals"
   );
-  const bestSellersItems = items.filter(
+  const bestSellersItems = items?.filter(
     (item) => item?.attributes?.category === "bestSellers"
   );
 
